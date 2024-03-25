ad0d7b61ced1cffbfd4b06784f8bac0b56b38e2b
lizonye <160551786+lizonye@users.noreply.github.com>
2024-03-25 19:33:49 -0400

    Update ForkingHomeworkRfile.R
    the file to be modified contains tidyverse coding to alter a data set.  The team member who executes the fork is to add data.table code that will produce the same output.  (Do not run or execute the code, you are only to enter the code).

--- ForkingHomeworkRfile.R
+++ ForkingHomeworkRfile.R
@@ -1,12 +1,13 @@
 library(tidyverse)
 library(dplyr)
 library(data.table)
-
+diamonds <- as.data.table(diamonds)
+head(diamonds[, .(carat, cut, price)][price > 7000 & price < 10000 & cut == "Premium"][order(-carat)], 15)
 diamonds
 
-diamonds%>%
-  select(carat,cut,price)%>%
-  filter(cut == "Premium")%>%
-  filter(price > 7000 & price < 10000)%>%
-  arrange(desc(carat))%>%
-  slice(1:20)
+#diamonds%>%
+ # select(carat,cut,price)%>%
+  #filter(cut == "Premium")%>%
+  #filter(price > 7000 & price < 10000)%>%
+  #arrange(desc(carat))%>%
+  #slice(1:20)
